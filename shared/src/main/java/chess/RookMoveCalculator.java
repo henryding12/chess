8b75686636f98eeebd20d55814ed3a87411f24ec
henryding <henryding12@gmail.com>
2026-09-19 11:09:06 -0600

    impl(p0) set up the boilerplate for RookMoveCalculator

--- shared/src/main/java/chess/RookMoveCalculator.java
+++ shared/src/main/java/chess/RookMoveCalculator.java
@@ -7,25 +7,40 @@ import java.util.List;
 public class RookMoveCalculator implements PieceMovesCalculator{
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        Collection<ChessMove> moves = new ArrayList<>();
-        int r = myPosition.getRow();
-        int c = myPosition.getColumn();
+        Collection<ChessMove> possibleMoves = new ArrayList<>();
+        Collection<ChessPosition> potentialPositions = new ArrayList<>();
+        // start position variables
+        int myPositionRow = myPosition.getRow();
+        int  myPositionColumn = myPosition.getColumn();
+        ChessGame.TeamColor myPositionColor = board.getPiece(myPosition).getTeamColor();
 
-        int upDist = 8 - r;
-        for (int i = 0; i < 5; i++) {
-            if (i == 0) {
-                // move up
-                for
+
+        int upDistance = 8 - myPositionRow;
+        int downDistance = myPositionRow - 1;
+        int[][] rookArgs = {{upDistance, 1}, {downDistance, -1}};
+        for (var arg : rookArgs) {
+            for (int i = 1; i < arg[0]; i=i+arg[1]) {
+                ChessPosition newPosition = new ChessPosition(myPositionRow + i*arg[1], myPositionColumn);
+                if (board.getPiece(newPosition) == null) {
+                    potentialPositions.add(newPosition);
+                } else {
+                    ChessGame.TeamColor newPositionColor = board.getPiece(newPosition).getTeamColor();
+                    if (newPositionColor != myPositionColor) {
+                        potentialPositions.add(newPosition);
+                        break; // exits since can't move further after taking the enemy piece
+                    } else {
+                        // friendly piece in the spot
+                        break; // exits because no friendly fire allowed
+                    }
+                }
+
             }
         }
-        for (int i = 0; i < upDist + 1; i++) {
-            r = r + i;
+
+        for (ChessPosition potentialPosition: potentialPositions){
+            ChessMove newMove = new ChessMove(myPosition, potentialPosition, null);
+            possibleMoves.add(newMove);
         }
-
-
-        ChessPosition newPos = new ChessPosition(r, c);
-        ChessMove newMove = new ChessMove(myPosition, newPos, null);
-        moves.add(newMove);
-        return moves;
+        return possibleMoves;
     }
 }
